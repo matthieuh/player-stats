65d9d5721387c5ebf4ec3487437292274066781b
Matthieu Hostache <matthieu.hostache@gmail.com>
2022-06-12 15:58:01 +0200

    feat: display wins and loses nb

--- src/pages/players/[id]/matches.tsx
+++ src/pages/players/[id]/matches.tsx
@@ -5,6 +5,7 @@ import GET_ALL_PLAYER_IDS from 'queries/getAllPlayerIds';
 import GET_ALL_PLAYERS from 'queries/getAllPlayers';
 import GET_ALL_MATCHES from 'queries/getAllMatches';
 import PlayerMatchesList from 'components/PlayerMatchesList';
+import { useMemo } from 'react';
 
 type PlayerDetailsProps = {
   player: Player;
@@ -12,11 +13,24 @@ type PlayerDetailsProps = {
 };
 
 const PlayerDetails: NextPage<PlayerDetailsProps> = ({ player, matches }) => {
+  const winsNb = useMemo(
+    () => matches.filter((m) => m.winner.id === player.id).length,
+    [player, matches]
+  );
+  const losesNb = useMemo(() => matches.length - winsNb, [winsNb, matches]);
+
   return (
     <div>
-      <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:truncate pb-6">
-        {player.firstname} {player.lastname} matches
-      </h2>
+      <div className="flex gap-2 items-baseline pb-6">
+        <h2 className="flex-1 text-2xl font-bold leading-7 text-gray-900 sm:truncate">
+          {player.firstname} {player.lastname} matches:
+        </h2>
+        <p className="px-4">
+          <span className="text-green-600">{winsNb}W</span>
+          <span className="text-slate-400"> / </span>
+          <span className="text-red-600">{losesNb}L</span>
+        </p>
+      </div>
       <div className="overflow-x-auto">
         <PlayerMatchesList player={player} matches={matches} />
       </div>
