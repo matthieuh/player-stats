1e265705c6325c24b2cc06eda0ea947fff861a85
Matthieu Hostache <matthieu.hostache@gmail.com>
2022-06-12 15:58:29 +0200

    test: add tests for formatDate and formatDuration

--- src/utils/format.test.ts
+++ src/utils/format.test.ts
@@ -1,4 +1,11 @@
-import { formatAge, formatHeight, formatWeight, formatNumber } from 'utils/format';
+import {
+  formatAge,
+  formatHeight,
+  formatWeight,
+  formatNumber,
+  formatDate,
+  formatDuration,
+} from 'utils/format';
 
 describe('utils/format', () => {
   describe('formatAge', () => {
@@ -29,6 +36,22 @@ describe('utils/format', () => {
       expect(formatNumber(100)).toBe('100');
       expect(formatNumber(1000)).toBe('1,000');
       expect(formatNumber(1000000)).toBe('1,000,000');
+    });
+  });
+
+  describe('formatDate', () => {
+    test('should work as expected', () => {
+      expect(formatDate('2021-05-16T13:26:00.000Z')).toBe('5/16/2021');
+      expect(formatDate('2022-12-02T13:26:00.000Z')).toBe('12/2/2022');
+      expect(formatDate('1990-10-27T13:26:00.000Z')).toBe('10/27/1990');
+    });
+  });
+
+  describe('formatDuration', () => {
+    test('should work as expected', () => {
+      expect(formatDuration('2021-05-16T23:26:00.000Z', '2021-05-16T23:26:00.000Z')).toBe('0h 0min');
+      expect(formatDuration('2022-12-02T23:26:00.000Z', '2021-05-16T14:12:00.000Z')).toBe('13569h 14min');
+      expect(formatDuration('1990-10-27T04:12:00.000Z', '1990-10-27T12:26:00.000Z')).toBe('8h 13min');
     });
   });
 });
